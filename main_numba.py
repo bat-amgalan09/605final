5ccfe2929603d9eed60b4dcbe9f8df80be050198
bat-amgalan09 <88327762+bat-amgalan09@users.noreply.github.com>
2025-05-11 20:44:40 -0400

    Update main_numba.py

--- main_numba.py
+++ main_numba.py
@@ -1,4 +1,6 @@
 from train_numba import train_with_numba
 
 if __name__ == "__main__":
-    train_with_numba(limit=3000)
+    train_with_numba(limit=10000)
+    metrics = train_with_numba()
+    plot_metrics(*metrics, save_path="gpu_metrics_numba.png")
