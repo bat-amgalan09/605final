19a19bdb755bbcf3e2eb6150aad8fe77c4fadf57
bat-amgalan09 <88327762+bat-amgalan09@users.noreply.github.com>
2025-05-11 21:28:03 -0400

    Update main_numba.py

--- main_numba.py
+++ main_numba.py
@@ -3,5 +3,4 @@ from visuals import plot_metrics
 
 if __name__ == "__main__":
     train_with_numba(limit=10000)
-    metrics = train_with_numba()
-    plot_metrics(*metrics, save_path="gpu_metrics_numba.png")
+
