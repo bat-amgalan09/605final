d44179ab45f671ba35738dbec6184c16427fb4f2
bat-amgalan09 <88327762+bat-amgalan09@users.noreply.github.com>
2025-05-11 21:30:29 -0400

    Update main_accelerator.py

--- main_accelerator.py
+++ main_accelerator.py
@@ -3,5 +3,3 @@ from visuals import plot_metrics
 
 if __name__ == "__main__":
     train_with_accelerator(limit=10000)
-    metrics = train_with_accelerator()
-    plot_metrics(*metrics, save_path="hf_accelerator.png")
