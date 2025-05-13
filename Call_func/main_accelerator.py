38bde8393262a66aa93781bb9e921fcdf408e72d
bat-amgalan09 <88327762+bat-amgalan09@users.noreply.github.com>
2025-05-13 02:09:58 -0400

    Update main_accelerator.py

--- Call_func/main_accelerator.py
+++ Call_func/main_accelerator.py
@@ -2,4 +2,4 @@ from Accelerator import train_with_accelerator
 from visuals import plot_metrics
 
 if __name__ == "__main__":
-    train_with_accelerator(limit=10000)
+    train_with_accelerator(limit=100000)
